8658037016fda98d4f804b84860ae4f52399a2f3
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-01 18:09:50 -0800

    changed the value of the road(envelope)

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -1,11 +1,11 @@
 class Envelope {
-    constructor(skeleton, width)    {
+    constructor(skeleton, width, roundness = 1)    {
         //skeleton = segments between two points 
         this.skeleton = skeleton;
         //using private function generatePolygon with the width in the arguments
-        this.poly = this.#generatePolygon(width);
+        this.poly = this.#generatePolygon(width, roundness);
     }
-    #generatePolygon(width) {
+    #generatePolygon(width, roundness) {
         //deconstructing skeleton to get the points value 
         const { p1, p2 } = this.skeleton;
         const radius = width / 2;
@@ -16,12 +16,14 @@ class Envelope {
         const alpha_ccw = alpha - Math.PI / 2;
         const points = [];
         //controlling the angle by adding the points step by step 
-        const step = Math.PI / 10;
+        const step = Math.PI / Math.max(1, roundness);
+        //make sure that the starting angle meets the end angle for the for-loop
+        const eps = step / 2;
          //getting the points all around the point1 and point2 starting from alpha_ccw angle to alpha_cw angle
-        for (let i = alpha_ccw; i <= alpha_cw; i += step)    {
+        for (let i = alpha_ccw; i <= alpha_cw + eps; i += step)    {
             points.push(translate(p1, i, radius));
         }
-        for (let i = alpha_ccw; i <= alpha_cw; i += step)    {
+        for (let i = alpha_ccw; i <= alpha_cw + eps; i += step)    {
             //adding 180 degree to the angle b/c point2 is apposite of the point 1 
             points.push(translate(p2, Math.PI + i, radius));
         }
