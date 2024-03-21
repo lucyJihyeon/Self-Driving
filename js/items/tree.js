79d77ede21160db81bcefcb15de73669339f7502
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-20 22:10:39 -0700

    create a segment from the tree to the view point

---
+++ js/items/tree.js
@@ -0,0 +1,14 @@
+class Tree {
+    constructor(center, size)   {
+        this.center = center;
+        //size of the base 
+        this.size = size;
+    }
+    draw(ctx, viewPoint)   {
+        const diff = subtract(this.center, viewPoint);
+        this.center.draw(ctx, { size: this.size, color: "green"});
+        //offset 
+        const top = add(this.center, diff);
+        new Segment(this.center, top).draw(ctx);
+    }
+}
