dda7e541750c775a2c1ebe3c23c72b689a9a371a
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-20 23:06:52 -0700

    added noisy radius around the central point of the tree randomly to create a pointy-leaf-like tree

--- js/items/tree.js
+++ js/items/tree.js
@@ -1,15 +1,42 @@
 class Tree {
-    constructor(center, size, heightCoef = 0.3)   {
-        this.center = center;
-        //size of the base 
-        this.size = size;
-        this.heightCoef = heightCoef;
+  constructor(center, size, heightCoef = 0.3) {
+    this.center = center;
+    //size of the base
+    this.size = size;
+    this.heightCoef = heightCoef;
+  }
+
+  //priviate method to generate level to the tree
+  #generateLevel(point, size) {
+    const points = [];
+    const rad = size / 2;
+    //loop through each angle(pi / 16) in a circle up to (360 degrees)
+    for (let a = 0; a < Math.PI * 2; a += Math.PI / 16) {
+      //add variability to the radius of each point generated around the central point.
+      const kindOfRandom = Math.cos(((a + this.center.x) * size) % 17) ** 2;
+      //create noisy radius by interpolating between 0.5,1 and a random value of t
+      const noisyRadius = rad * lerp(0.5, 1, kindOfRandom);
+      //calculate the new positioned after applying the angle and noisy radius
+      points.push(translate(point, a, noisyRadius));
     }
-    draw(ctx, viewPoint)   {
-        const diff = subtract(this.center, viewPoint);
-        this.center.draw(ctx, { size: this.size, color: "green"});
-        //offset 
-        const top = add(this.center, scale(diff ,this.heightCoef));
-        new Segment(this.center, top).draw(ctx);
+    return new Polygon(points);
+  }
+  draw(ctx, viewPoint) {
+    const diff = subtract(this.center, viewPoint);
+    //offset
+    const top = add(this.center, scale(diff, this.heightCoef));
+    const levelCount = 7;
+    for (let level = 0; level < levelCount; level++) {
+      // t value used for linear interpolation
+      // t = ratio to determine the intermediate value between two end points (top and bottom of the tree)
+      const t = level / (levelCount - 1);
+      const point = lerp2D(this.center, top, t);
+      //change the color as they go higher
+      const color = "rgb(30," + lerp(50, 200, t) + ",70)";
+      //change the size of the point as they go heigher
+      const size = lerp(this.size, 40, t);
+      const poly = this.#generateLevel(point, size);
+      poly.draw(ctx, { fill: color, stroke: "rgba(0,0,0,0)" });
     }
+  }
 }
