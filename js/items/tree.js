ddb13b9018e8d91c9734fd97dcaa27c07919aa01
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-20 22:21:25 -0700

    added heigth coefficient as a constructor with a default value of 0.3

--- js/items/tree.js
+++ js/items/tree.js
@@ -1,14 +1,15 @@
 class Tree {
-    constructor(center, size)   {
+    constructor(center, size, heightCoef = 0.3)   {
         this.center = center;
         //size of the base 
         this.size = size;
+        this.heightCoef = heightCoef;
     }
     draw(ctx, viewPoint)   {
         const diff = subtract(this.center, viewPoint);
         this.center.draw(ctx, { size: this.size, color: "green"});
         //offset 
-        const top = add(this.center, scale(diff ,0.5));
+        const top = add(this.center, scale(diff ,this.heightCoef));
         new Segment(this.center, top).draw(ctx);
     }
 }
