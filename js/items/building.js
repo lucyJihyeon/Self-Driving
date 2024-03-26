d9cdf1ac375045e8eca0b8414280d028f02e6a53
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-26 00:36:26 -0700

    get rid of the black line on the celing side of the building

--- js/items/building.js
+++ js/items/building.js
@@ -67,7 +67,7 @@ class Building {
     for (const side of sides) {
       side.draw(ctx, { fill: "white", stroke: "#AAA" });
     }
-    ceiling.draw(ctx, { fill: "white", stroke: "#AAA" });
+    ceiling.draw(ctx, { fill: "white", stroke: "white", lineWidth: 6 });
     for (const poly of roofPolys) {
       poly.draw(ctx, { fill: "#D44", stroke: "#C44", lineWidth : 8, join: "round" 
     });
