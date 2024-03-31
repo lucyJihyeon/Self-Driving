6a53e15e93b52b4b06a9a3b98ef0c6286425c500
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-30 19:12:13 -0700

    refactoring markings

--- js/markings/crossing.js
+++ js/markings/crossing.js
@@ -1,18 +1,6 @@
-class Crossing {
+class Crossing extends Marking {
     constructor(center, directionVector, width, height) {
-        this.center = center;
-        this.directionVector = directionVector;
-        this.width = width;
-        this.height = height;
-
-        //calculate the support segment that represent the physical structure for the stop sign (height)
-        this.support = new Segment (
-            //the segment that made from the center(projection) of the nearest segment and directionvector that aligns with the center of the road segment polygon 
-            translate(center, angle(directionVector), height / 2),
-            translate(center, angle(directionVector), -height / 2),
-        );
-        //wrap around the support segment to create a polygon 
-        this.poly = new Envelope(this.support, width, 0).poly;
+       super(center, directionVector, width, height);
         //border = 3rd item of the poly's segments which is the top side of the polygon
         //first item = bottom side of the polygon so that the car is aware of both of the borders 
         this.border = [this.poly.segments[0], this.poly.segments[2]];
